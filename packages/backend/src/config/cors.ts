11c568a123d0a3b0418fc4e25628809fd4d17f55
gustavonunesx <gu.nunesx@gmail.com>
2026-05-21 11:29:28 -0300

    fix: normalize CORS origin to strip trailing slash

--- packages/backend/src/config/cors.ts
+++ packages/backend/src/config/cors.ts
@@ -1,8 +1,16 @@
 import cors from 'cors';
 import { env } from './env';
 
+const allowedOrigin = env.FRONTEND_URL.replace(/\/$/, '');
+
 export const corsMiddleware = cors({
-  origin: env.FRONTEND_URL,
+  origin: (origin, callback) => {
+    if (!origin || origin.replace(/\/$/, '') === allowedOrigin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-API-Key'],
